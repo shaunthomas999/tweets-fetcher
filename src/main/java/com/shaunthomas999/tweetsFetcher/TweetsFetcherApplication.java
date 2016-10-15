720e542cbce57b8199516627f2f399c0e5600214
Shaun Thomas <thomas@innoscale.de>
2016-10-15 11:36:09 +0200

    Created application base

---
+++ src/main/java/com/shaunthomas999/tweetsFetcher/TweetsFetcherApplication.java
@@ -0,0 +1,12 @@
+package com.shaunthomas999.tweetsFetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetsFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetsFetcherApplication.class, args);
+	}
+}
